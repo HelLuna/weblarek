2ffab87ab4b9cb4e1c82a946f0ce1cf52d372e33
Tatiana Volzhanina <tanyavolzh@mail.ru>
2026-05-17 23:17:32 +0300

    Add class Catalog

---
+++ src/components/models/Catalog.ts
@@ -0,0 +1,40 @@
+import { IProduct } from "../../types/index.ts";
+
+export class Catalog {
+  protected products: IProduct[];
+  protected selectedProduct: IProduct | null;
+
+  constructor(
+    products: IProduct[] = [],
+    selectedProduct: IProduct | null = null,
+  ) {
+    this.products = [...products];
+    this.selectedProduct = selectedProduct;
+  }
+
+  setProducts(products: IProduct[]): void {
+    this.products = [...products];
+  }
+
+  getProducts(): IProduct[] {
+    return this.products;
+  }
+
+  getProductById(id: string): IProduct {
+    const product = this.products.find((product) => product.id === id);
+
+    if (product) {
+      return product;
+    }
+
+    throw new Error(`Продукт с id ${id} не найден.`);
+  }
+
+  setSelectedProduct(product: IProduct): void {
+    this.selectedProduct = product;
+  }
+
+  getSelectedProduct(): IProduct | null {
+    return this.selectedProduct;
+  }
+}
