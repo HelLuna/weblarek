b404c8d525743b2f221f7cfd5fd646b772e2fbec
Tatiana Volzhanina <tanyavolzh@mail.ru>
2026-05-17 23:17:52 +0300

    Add class Basket

---
+++ src/components/models/Basket.ts
@@ -0,0 +1,47 @@
+import { IProduct } from "../../types/index.ts";
+
+export class Basket {
+  protected items: IProduct[];
+
+  constructor(items: IProduct[] = []) {
+    this.items = [...items];
+  }
+
+  getItems(): IProduct[] {
+    return this.items;
+  }
+
+  addItem(item: IProduct): void {
+    this.items.push(item);
+  }
+
+  removeItem(item: IProduct): void {
+    const index = this.items.findIndex((product) => product.id === item.id);
+
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+
+  clear(): void {
+    this.items = [];
+  }
+
+  getTotalPrice(): number {
+    return this.items.reduce((total, item) => total + (item.price ?? 0), 0);
+  }
+
+  getAmount(): number {
+    return this.items.length;
+  }
+
+  hasItem(id: string): boolean {
+    const item = this.items.find((item) => item.id === id);
+
+    if (item) {
+      return true;
+    }
+
+    return false;
+  }
+}
